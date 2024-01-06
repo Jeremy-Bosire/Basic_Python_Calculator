8ef151b2107698139ebd135391f44b6530f58200
Jeremy Bosire <114059541+Jeremy-Bosire@users.noreply.github.com>
2024-01-06 23:53:16 +0300

    I had to push something

--- hello.py
+++ hello.py
@@ -1,2 +1,3 @@
 print("Today I didn't really have something to do, so I have just decided to push this basic code")
 print("Also today I don't have something to push. I only read about test drven development in python")
+print("I also did not push much today and I really don't want to burn my coding streaks")
